518c1960cf8528752f387239c857d44e2e0cd295
Dmytro Nykorovych <dev.dmytronykorovych@gmail.com>
2025-04-21 12:51:58 +0200

    add add throw new Error for default

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -35,7 +35,7 @@ function transformStateWithClones(state, actions) {
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     result.push({ ...currentState });
